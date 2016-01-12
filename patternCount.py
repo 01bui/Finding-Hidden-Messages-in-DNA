8f44e2e2fef0166068a0b9203d000426d9111ec8
vbui <01bui@cardinalmail.cua.edu>
2016-01-12 01:16:21 -0500

    Add function to read DNA file

--- patternCount.py
+++ patternCount.py
@@ -4,13 +4,21 @@ def patternCount(Text, Pattern):
     count = 0
     textList = list(Text)
     patternList = list(Pattern)
+    print len(textList)
+    print len(patternList)
     for i in range(0,len(textList)-len(patternList)+1,1):
         if textList[i:i+len(patternList)] == patternList:
             count += 1
     return count
 
+def readData(filename):
+    with open(filename, 'r') as f:
+        f.readline() # Skip input line
+        Text = f.readline()
+        Pattern = f.readline()
+        return Text.strip(), Pattern.strip()
+
 if __name__ == "__main__":
-    Text = "GCGCG"
-    Pattern = "GCG"
+    Text, Pattern = readData('PatternCount.txt')
     resultCount = patternCount(Text, Pattern)
     print resultCount
