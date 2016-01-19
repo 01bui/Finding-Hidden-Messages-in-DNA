a33115b965459cd37ebfc0505c1985868da73c6d
vbui <01bui@cardinalmail.cua.edu>
2016-01-18 20:43:11 -0500

    Support read data from text file

--- computingFrequencies.py
+++ computingFrequencies.py
@@ -1,24 +1,28 @@
-from numberToPattern import *
-from patternToNumber import *
+from numberToPattern import numberToPattern
+from patternToNumber import patternToNumber, symbolToNumber
 
 def computingFreqs(Text, k):
     textList = list(Text)
     frequencyArray = []
     for i in range(4**k-1+1):
         frequencyArray.insert(i, 0)
-    #print frequencyArray
     for i in range(len(textList)-k+1):
         pattern = textList[i:i+k]
         j = patternToNumber(pattern)
-        #print pattern, j, frequencyArray[j]
-        #print frequencyArray
         x = frequencyArray[j] + 1
-        #print x
         frequencyArray[j] += 1
     return frequencyArray
 
+def read_data(filename):
+    with open(filename, 'r') as f:
+        Text = f.readline()
+        k = f.readline()
+        return Text.strip(), int(k)
+
 if __name__ == "__main__":
-    Text = "ACGCGGCTCTGAAA"
-    k = 2
+    Text, k = read_data('dataset_2994_5.txt')
     result = computingFreqs(Text, k)
-    print result
+    out = []
+    for i in result:
+        out.append(str(i))
+    print " ".join(out)
